7557ffcda68cc5a4dbc9eb84f8654f1dc6b3acf0
Rachile <Rachile@Rachile-PC>
2019-11-05 13:41:29 +0100

    demo

--- src/main/java/com/example/General.java
+++ src/main/java/com/example/General.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class General {   
-   /* public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
          SpringApplication.run(General.class, args);
-    }*/
+    }
 }
